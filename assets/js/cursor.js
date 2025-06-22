3ed6df03a65c810842a944ef551a5d5942f825d3
sahil-7878 <mrsahil8316@gmail.com>
2025-06-22 17:35:58 +0530

    Add files via upload

---
+++ assets/js/cursor.js
@@ -0,0 +1,9 @@
+options = {
+    "outerStyle": "circle",
+    "hoverEffect": "pointer-blur",
+    "hoverItemMove": false,
+    "defaultCursor": false,
+    "outerWidth": 41,
+    "outerHeight": 41
+}
+magicMouse(options);
